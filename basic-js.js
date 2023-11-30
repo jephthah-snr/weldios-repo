6dc579616ddd3d23466015d09f951b622d357efd
jephthah-snr <60290652+jephthah-snr@users.noreply.github.com>
2023-11-30 11:06:59 +0100

    fixed an error in the user.service file

---
+++ basic-js.js
@@ -0,0 +1,17 @@
+// console.log("hello weldios");
+
+let result;
+
+let data = ["apple", "banana", "success", 1000000]
+            // 0       1           2          3
+//console.log(data[2])
+// number ++ = number = number + 1
+for(let number = 0; number < 10; number++){
+    console.log(number)
+}
+
+// string = "hello",
+// number = 3214324,
+// boolean = true/false
+// array = [34124,345143,43513, "names", "hello"]
+
